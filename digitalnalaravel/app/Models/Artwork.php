331be19b1d575965e0d3c47a462b147dc1cc796d
kristinarbc <kristinapantelickv@gmail.com>
2025-10-04 18:53:50 +0200

    Eloquent relacije u modelima

--- digitalnalaravel/app/Models/Artwork.php
+++ digitalnalaravel/app/Models/Artwork.php
@@ -21,4 +21,13 @@ class Artwork extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    public function images() {
+        return $this->hasMany(Image::class);
+    }
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
--- digitalnalaravel/app/Models/Image.php
+++ digitalnalaravel/app/Models/Image.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
+use App\Models\Artwork;
 
 class Image extends Model
 {
     protected $fillable = ['name', 'path', 'category_id'];
 
+    public function artwork() {
+        return $this->belongsTo(Artwork::class);
+    }
+    
     public function category() {
         return $this->belongsTo(Category::class);
     }
+    
 }
